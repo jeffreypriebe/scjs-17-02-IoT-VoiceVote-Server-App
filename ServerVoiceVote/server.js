8eff977a7dd438de12c0e8dd12b2c56a5002ed20
Jeffrey Priebe <jeffreypriebe@gmail.com>
2017-02-10 21:00:52 -0800

    Initial socket server

---
+++ ServerVoiceVote/server.js
@@ -0,0 +1,31 @@
+const express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	socketio = require('socket.io'),
+	io = socketio(server);
+
+io.listen(30809);
+console.log('socket listening on 30809');
+
+io.on('connection', socket => {
+	
+	// IoT creates a room sending the room name
+	socket.on('join', data => {
+		const { name } = data;
+		
+		console.log(`Joining room: ${name}`);
+		
+		socket.join(name);
+		socket.room = name;
+		
+		// setTimeout(function() {
+		// 	console.log('emitting');
+		// 	io.sockets.in(name).emit('message', { message: 'From server'} );
+		// }, 1000);
+	});
+	
+	socket.on('message', data => {
+		console.log(`Emitting: 'message'; ${JSON.stringify(data)}`);
+		socket.broadcast.to(socket.room).emit('message', data);
+	});
+});
