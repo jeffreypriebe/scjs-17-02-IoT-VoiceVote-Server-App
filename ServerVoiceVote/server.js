d83950a304aa540d0cba1ce5edb9c4060caf73ba
Jeffrey Priebe <jeffreypriebe@gmail.com>
2017-02-22 21:05:21 -0800

    Remove arrow functions, consts

--- ServerVoiceVote/server.js
+++ ServerVoiceVote/server.js
@@ -1,4 +1,4 @@
-const express = require('express'),
+var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	socketio = require('socket.io'),
@@ -7,19 +7,19 @@ const express = require('express'),
 io.listen(30809);
 console.log('socket listening on 30809');
 
-io.on('connection', socket => {
+io.on('connection', function(socket) {
 	
-	socket.on('join', data => {
-		const { name } = data;
+	socket.on('join', function(data) {
+		var name = data.name;
 		
-		console.log(`Joining room: ${name}`);
+		console.log('Joining room: ' + name);
 		
 		socket.join(name);
 		socket.room = name;
 	});
 	
-	socket.on('message', data => {
-		console.log(`Emitting in '${socket.room}': ${JSON.stringify(data)}`);
+	socket.on('message', function(data) {
+		console.log('Emitting in \'' + socket.room + '\': ' + JSON.stringify(data));
 		socket.broadcast.to(socket.room).emit('message', data);
 	});
 });
